4cebb83005533328a1024aedac47ec66640555bc
Andrey Shaposhnikov <droniiiandruxa@gmail.com>
2023-04-28 15:53:34 +0300

    UPD

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,125 +1,110 @@
 /**
+ * Libraries, services
+ */
+import { useCallback, useEffect, useState } from 'react';
+import { BallTriangle } from 'react-loader-spinner';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
+import { getPictures } from 'utils/pixabayAPI';
+/**
  * Components
  */
 import { Modal } from './Modal/Modal';
-import { getPictures } from 'utils/pixabayAPI';
+
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Button } from './Button/Button';
 import { StyledSection } from './App.styled';
-/**
- * Libraries
- */
-import { Component } from 'react';
-import { BallTriangle } from 'react-loader-spinner';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.min.css';
-export class App extends Component {
-  state = {
-    modalShown: false,
-    images: [],
-    searchQuery: '',
-    totalImageCount: 0,
-    page: 1,
-    perPage: 12,
-    currentImage: {
-      largeImageUrl: '',
-      alt: '',
-    },
-    isLoading: false,
-    error: '',
-  };
-  componentDidUpdate(prevProps, prevState) {
-    if (
-      prevState.searchQuery !== this.state.searchQuery ||
-      prevState.page !== this.state.page
-    ) {
-      this.getData();
-    }
-    if (
-      this.state.totalImageCount === this.state.images.length &&
-      this.state.totalImageCount !== 0 &&
-      prevState.images.length !== this.state.images.length
-    ) {
-      console.log(this.state.totalImageCount, this.state.images.length);
-      toast(`That's all images on this request 😥`);
-    }
-  }
-  /**
-   * Own functions
-   */
-  getData = () => {
-    const { page, perPage, searchQuery } = this.state;
-    this.setState({ isLoading: true });
+
+export const App = () => {
+  const [modalShown, setModalShown] = useState(false);
+  const [images, setImages] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [totalImageCount, setTotalImageCount] = useState(0);
+  const [page, setPage] = useState(1);
+  const [perPage] = useState(12);
+  const [largeImageUrl, setLargeImageUrl] = useState('');
+  const [alt, setAlt] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getData = useCallback(() => {
+    setIsLoading(true);
     getPictures(searchQuery, page, perPage)
       .then(({ data }) => {
-        this.setState(prevState => ({
-          images: [...prevState.images, ...data.hits],
-        }));
-        this.setState({ totalImageCount: data.totalHits });
+        setImages(prevState => [...prevState, ...data.hits]);
+        setTotalImageCount(data.totalHits);
         if (page === 1) {
           toast(`Wow! We found ${data.totalHits} images for you 😍`);
         }
       })
-      .finally(() => this.setState({ isLoading: false }));
-  };
-  onSearchSubmit = e => {
+      .finally(() => setIsLoading(false));
+  }, [page, perPage, searchQuery]);
+
+  useEffect(() => {
+    if (!searchQuery) {
+      return;
+    }
+    getData();
+  }, [searchQuery, page, getData]);
+  useEffect(() => {
+    if (totalImageCount === images.length && totalImageCount !== 0) {
+      toast(`That's all images on this request 😥`);
+    }
+  }, [images, totalImageCount]);
+
+  /**
+   * Own functions
+   */
+  const onSearchSubmit = e => {
     e.preventDefault();
-    const searchQuery = e.target.elements.searchInput.value;
-    this.setState({
-      searchQuery,
-      images: [],
-      page: 1,
-    });
-  };
-  onLoadMoreBtnClick = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
-  };
-  toggleModal = (largeImageUrl, alt) => {
-    this.setState(prevState => ({ modalShown: !prevState.modalShown }));
-    this.setState({ currentImage: { largeImageUrl, alt } });
-  };
-  render() {
-    return (
-      <div>
-        <Searchbar formSubmitHandler={this.onSearchSubmit} />
+    const searchValue = e.target.elements.searchInput.value;
 
-        {this.state.isLoading && (
-          <StyledSection $loader>
-            <BallTriangle
-              height={100}
-              width={100}
-              radius={5}
-              color="#3f51b5"
-              ariaLabel="ball-triangle-loading"
-              wrapperClass={{}}
-              wrapperStyle=""
-              visible={true}
-            />
-          </StyledSection>
-        )}
-        {this.state.images.length > 0 && (
-          <StyledSection>
-            <ImageGallery
-              data={this.state.images}
-              onItemClick={this.toggleModal}
-            />
-            {this.state.images.length !== this.state.totalImageCount && (
-              <Button clickHandler={this.onLoadMoreBtnClick} />
-            )}
-          </StyledSection>
-        )}
-        {this.state.modalShown && (
-          <Modal onClose={this.toggleModal}>
-            <img
-              src={this.state.currentImage.largeImageUrl}
-              alt={this.state.currentImage.alt}
-            />
-          </Modal>
-        )}
+    setSearchQuery(searchValue);
+    setImages([]);
+    setPage(1);
+  };
+  const onLoadMoreBtnClick = () => {
+    setPage(prevState => prevState + 1);
+  };
+  const toggleModal = (largeImageUrl, alt) => {
+    setModalShown(prevState => !prevState);
+    setLargeImageUrl(largeImageUrl);
+    setAlt(alt);
+  };
 
-        <ToastContainer />
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Searchbar formSubmitHandler={onSearchSubmit} />
+
+      {isLoading && (
+        <StyledSection $loader>
+          <BallTriangle
+            height={100}
+            width={100}
+            radius={5}
+            color="#3f51b5"
+            ariaLabel="ball-triangle-loading"
+            wrapperClass={{}}
+            wrapperStyle=""
+            visible={true}
+          />
+        </StyledSection>
+      )}
+      {images.length > 0 && (
+        <StyledSection>
+          <ImageGallery data={images} onItemClick={toggleModal} />
+          {images.length !== totalImageCount && (
+            <Button clickHandler={onLoadMoreBtnClick} />
+          )}
+        </StyledSection>
+      )}
+      {modalShown && (
+        <Modal onClose={toggleModal}>
+          <img src={largeImageUrl} alt={alt} />
+        </Modal>
+      )}
+
+      <ToastContainer />
+    </div>
+  );
+};
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,37 +1,112 @@
-import { Component } from 'react';
+/**
+ * Libraries, services
+ */
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { createPortal } from 'react-dom';
+/**
+ * Components
+ */
 import { StyledModal, StyledOverlay } from './Modal.styled';
+
 const modalRoot = document.querySelector('#modal-root');
 
-export class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.onEscapeClick);
-  }
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.onEscapeClick);
-  }
+export const Modal = ({ onClose, children }) => {
+  // componentDidMount() {
+  //   window.addEventListener('keydown', this.onEscapeClick);
+  // }
+  useEffect(() => window.addEventListener('keydown', onEscapeOrOverlayClick));
 
-  onEscapeClick = ev => {
+  // componentWillUnmount() {
+  //   window.removeEventListener('keydown', this.onEscapeOrOverlayClick);
+  // }
+  useEffect(() => {
+    return () => {
+      window.removeEventListener('keydown', onEscapeOrOverlayClick);
+    };
+  });
+
+  /**
+   * Own methods
+   */
+  const onEscapeOrOverlayClick = ev => {
     if (ev.code === 'Escape' || ev.target === ev.currentTarget) {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  render() {
-    return createPortal(
-      <StyledOverlay onClick={this.onEscapeClick} id="backdrop">
-        <StyledModal>{this.props.children}</StyledModal>
-      </StyledOverlay>,
-      modalRoot
-    );
-  }
-}
+  return createPortal(
+    <StyledOverlay onClick={onEscapeOrOverlayClick} id="backdrop">
+      <StyledModal>{children}</StyledModal>
+    </StyledOverlay>,
+    modalRoot
+  );
+};
 
 Modal.propTypes = {
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.element),
     PropTypes.element,
   ]),
-  onClose: PropTypes.func,
 };
+
+/**
+ * Class method
+ */
+// import { Component } from 'react';
+// import PropTypes from 'prop-types';
+// import { createPortal } from 'react-dom';
+// import styled from 'styled-components';
+
+// const modalRoot = document.querySelector('#modal-root');
+
+// export class Modal extends Component {
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.onEscapeClick);
+//   }
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.onEscapeClick);
+//   }
+
+//   onEscapeClick = ev => {
+//     if (ev.code === 'Escape') {
+//       this.props.onClose();
+//     }
+//   };
+//   onOverlayClick = ev => {
+//     if (ev.target === ev.currentTarget) {
+//       this.props.onClose();
+//     }
+//   };
+//   render() {
+//     return createPortal(
+//       <StyledOverlay onClick={this.onOverlayClick} id="backdrop">
+//         <StyledModal>{this.props.children}</StyledModal>
+//       </StyledOverlay>,
+//       modalRoot
+//     );
+//   }
+// }
+
+// Modal.propTypes = {
+//   children: PropTypes.oneOfType([
+//     PropTypes.arrayOf(PropTypes.element),
+//     PropTypes.element,
+//   ]),
+// };
+// const StyledOverlay = styled.div`
+//   position: fixed;
+//   top: 0;
+//   left: 0;
+//   width: 100vw;
+//   height: 100vh;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   background-color: rgba(0, 0, 0, 0.8);
+//   z-index: 1200;
+// `;
+// const StyledModal = styled.div`
+//   max-width: calc(100vw - 48px);
+//   max-height: calc(100vh - 24px);
+// `;
